66a6e485fa3932416633a4bb20c7035493214b2e
Camilo Rodriguez <camilorodriguezco7@gmail.com>
2023-06-01 21:25:47 -0500

    COmpontente de Formulario para el buscador, listado de noticias para el feed de noticias y Noticia, ya consulta api y muestra en pantalla

---
+++ src/components/ListadoNoticias.jsx
@@ -0,0 +1,38 @@
+import  Grid  from "@mui/material/Grid"
+import  Typography  from "@mui/material/Typography"
+import useNoticias from "../hooks/useNoticias"
+import Noticia from "./Noticia"
+
+const ListadoNoticias = () => {
+
+    const {noticias} = useNoticias()
+
+    console.log(noticias);
+
+  return (
+    <>
+        <Typography
+            textAlign={'center'}
+            marginY={5}
+            variant="h3"
+            component={'h2'}
+        >
+            Últimas Noticias
+        </Typography>
+        <Grid 
+            container
+            spacing={2}
+        >
+            {noticias.map(noticia=>(
+                <Noticia
+                    noticia={noticia}
+                    key={noticia.url}
+
+                />
+            ))}
+        </Grid>
+    </>
+  )
+}
+
+export default ListadoNoticias
